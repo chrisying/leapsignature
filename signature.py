3a980bd5818c536d10b3a63b52c4c8acab5369c2
Chris Ying <chrsyng@gmail.com>
2013-11-03 15:48:39 -0500

    Added signature base code

---
+++ signature.py
@@ -0,0 +1,93 @@
+################################################################################
+# Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               #
+# Leap Motion proprietary and confidential. Not for distribution.              #
+# Use subject to the terms of the Leap Motion SDK Agreement available at       #
+# https://developer.leapmotion.com/sdk_agreement, or another agreement         #
+# between Leap Motion and you, your company or other organization.             #
+################################################################################
+
+#Signature recogition code
+#Chris Ying 2013
+
+import Leap, sys
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+
+class SignatureListener(Leap.Listener):
+    def on_init(self, controller):
+        print "Initialized"
+
+    def on_connect(self, controller):
+        print "Connected"
+
+        # Enable gestures
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+
+    def on_disconnect(self, controller):
+        # Note: not dispatched when running in a debugger.
+        print "Disconnected"
+
+    def on_exit(self, controller):
+        print "Exited"
+
+    def on_frame(self, controller):
+        # Get the most recent frame and report some basic information
+        frame = controller.frame()
+
+        if not frame.hands.is_empty:
+            # Get the first hand
+            hand = frame.hands[0]
+
+            # Check if the hand has any fingers
+            fingers = hand.fingers
+            if not fingers.is_empty:
+                # Calculate the hand's average finger tip position
+                if len(fingers) in (1, 2):
+                	avg_pos = Leap.Vector()
+                	for finger in fingers:
+                   		avg_pos += finger.tip_position   	
+            		avg_pos /= len(fingers)
+                	coords = avg_pos.to_tuple()
+                	print "X-coord: %d, Y-coord: %d, Z-coord: %d" % (coords[0], coords[1], coords[2])
+            	else:
+            	  	print "There are %d fingers on frame" % len(fingers)
+            else:
+            	print "No fingers in frame"
+
+        else:
+			print "No hands in frame"
+
+    def state_string(self, state):
+        if state == Leap.Gesture.STATE_START:
+            return "STATE_START"
+
+        if state == Leap.Gesture.STATE_UPDATE:
+            return "STATE_UPDATE"
+
+        if state == Leap.Gesture.STATE_STOP:
+            return "STATE_STOP"
+
+        if state == Leap.Gesture.STATE_INVALID:
+            return "STATE_INVALID"
+
+def main():
+    # Create a sample listener and controller
+    listener = SignatureListener()
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    sys.stdin.readline()
+
+    # Remove the sample listener when done
+    controller.remove_listener(listener)
+
+
+if __name__ == "__main__":
+    main()
